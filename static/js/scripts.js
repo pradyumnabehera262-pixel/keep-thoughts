1ffb299cde70b1fa82272bfbb02e205f9bc232cd
Pradyumna Behera <pradyumnabehera262@gmail.com>
2026-08-05 15:57:31 +0530

    onclick event animation

---
+++ static/js/scripts.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("addThoughtBtn");
+const main = document.getElementById("main");
+
+button.addEventListener("click", function () {
+        main.classList.add("expanding");
+    });

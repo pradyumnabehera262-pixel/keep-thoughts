89408257626dc46cd837ac865ed4ad580814dc2c
Pradyumna Behera <pradyumnabehera262@gmail.com>
2026-08-06 15:48:08 +0530

    animation update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,12 @@
 const button = document.getElementById("addThoughtBtn");
 const main = document.getElementById("main");
+const backBtn = document.getElementById("backBtn");
 
 button.addEventListener("click", function () {
         main.classList.add("expanding");
+        console.log("Button clicked, expanding class added to main.");
     });
+
+backBtn.addEventListener("click", function (){
+    main.classList.remove("expanding");
+});
